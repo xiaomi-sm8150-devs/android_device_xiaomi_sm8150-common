e4293e01583976ec2774d1b676f5df3c3dc3481f
Danny Trunk <dtrunk90@gmail.com>
2022-01-28 15:37:29 +0100

    sm8150-common: Add shim for missing symbol in lib-imsvideocodec.so
    This is a squash of the following:
    
    Author:     Danny Trunk <dtrunk90@gmail.com>
    AuthorDate: 2022-01-28 15:37:29 +0100
    
        Fixes:
    
            E ims_media_jni: Error loading library lib-imsvideocodec.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system/product/lib64/lib-imsvideocodec.so"...
    
        Change-Id: I4364a1e1da2ae2120194f44c233f1f8b1cd0f7ac
    
    Author:     Bruno Martins <bgcngm@gmail.com>
    AuthorDate: 2022-09-21 12:15:09 +0100
    
        sdm845-common: Migrate to proper libgui shim
    
        It has been placed under hardware/lineage/compat.
    
        Change-Id: I3767d8a44cbd9af891fbac7a67380b205b414a37
    
    Author:     Cosmin Tanislav <demonsingur@gmail.com>
    AuthorDate: 2024-08-17 04:59:30 +0300
    
        sdm845-common: Do not manually build dependencies
    
        Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b
    
    Change-Id: I4364a1e1da2ae2120194f44c233f1f8b1cd0f7ac

--- extract-files.py
+++ extract-files.py
@@ -22,6 +22,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libgui_shim.so'),
     ('system_ext/lib/libwfdservice.so', 'system_ext/lib64/libwfdservice.so'): blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
