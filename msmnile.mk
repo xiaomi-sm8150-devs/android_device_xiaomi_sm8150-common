69da70a81a69dafc7a70113c66ecef2364ef8064
Pranav Vashi <neobuddy89@gmail.com>
2024-04-05 23:29:43 +0530

    sm6150-common: Set TARGET_PACKAGE_NAME for miui camera
    Change-Id: If6ad715b836d5650443002a41f789ebc5ec490f8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -134,6 +134,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
+$(call soong_config_set,camera,package_name,com.xiaomi.sessionparams.clientName)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
