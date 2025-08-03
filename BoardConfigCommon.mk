d2ed5842893feb93564292c4c3095a62a0310bca
armdebug <armdebug@protonmail.com>
2025-08-03 02:19:19 -0400

    sm8150-common: Revert "Build AIDL NXP NFC HAL"
    This reverts commit 384d3ddf48e7e4cf6f3f3a6e448cb49b8a1c8c29.
    
    Change-Id: I343467dd953fe5cfd83c61baef480de0c5dd8b8a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -228,7 +228,8 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 endif
 
 # VINTF
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
--- msmnile.mk
+++ msmnile.mk
@@ -249,7 +249,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.nxp \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     Tag
 
