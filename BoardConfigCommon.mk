67da5b7b4050b5cd66e9eb9e05465c73ffcdd2e9
numanmushtaq01 <naveenmushtaq3@gmail.com>
2026-02-15 03:42:52 +0530

    sm8150-common: Remove vendor/lineage device framework matrix inclusion

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -223,8 +223,7 @@ endif
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 ifeq ($(TARGET_HAS_FM),true)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_fm.xml
