Evolution-X-Devices/device_xiaomi_sm8150-common

sm8150-common: Remove dcvs init
   *not needed anymore

Change-Id: I321f9ca6773c82c6c1f63d3a4f81aa1fa030acb0
Signed-off-by: firebird11 <hbgassel@gmail.com>

msmnile.mk
@@ -445,8 +445,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
-    init.qti.dcvs.sh
+    init.qcom.usb.sh
 
 PRODUCT_PACKAGES += \
     init.qcom.power.rc \

xiaomi-sm8150-devs/android_device_xiaomi_sm8150-common

msmnile.mk
@@ -327,8 +327,7 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
-    init.qti.chg_policy.sh \
-    init.qti.dcvs.sh
+    init.qti.chg_policy.sh
 
 PRODUCT_PACKAGES += \
     init.qcom.power.rc \